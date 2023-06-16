97d99684a19674a30b445958328818d08d597510
Jenkins Course <jenkins@theconstruct.ai>
2023-06-16 13:17:04 +0000

    Exam repo files

---
+++ exam/takeoff_drone.sh
@@ -0,0 +1,7 @@
+# Start the ROS script that TAKES OFF the drone in the background
+# Capture the process ID of that script in TAKEOFF_ID
+# Wait for 15 seconds and then kill the process
+rostopic pub /drone/takeoff std_msgs/Empty '{}' &
+TAKEOFF_ID=$!
+sleep 15s
+kill $TAKEOFF_ID
